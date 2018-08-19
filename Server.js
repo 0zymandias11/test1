36000e50d05d9781d400d86dd4737b2d72a8a165
Flamer17 <32807736+Flamer17@users.noreply.github.com>
2018-08-19 17:18:43 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -27,37 +27,41 @@ res.json("adfsdfdsf");
 
 app.post('/Signin/:id',(req,res)=>
 {
+	var u_name;
 	const uid=req.params.id
 knex('users').where({
   id: uid
-}).select('id').then(data=>{
+}).select('id','name').then(data=>{
 	if(data[0]!=undefined)
 	{
 	if(data[0].id==uid){
 		d_id=uid;
+		u_name=data[0].name;
+		console.log(u_name);
 		//flag=true;
 
-knex('submissions').where({
+knex('ans').where({
   u_id: d_id
 }).select('u_id').then(data2=>{
 	if(data2[0]==undefined){
 
-		knex('submissions').insert({u_id: d_id}).then(data =>
+		knex('ans').insert({u_id: d_id ,name:u_name }).then(data =>
 	{
 		//console.log(data);
 	});
-		knex.select('*').from('submissions').then(data =>
+		knex.select('*').from('ans').then(data =>
 	{
 		console.log(data);
 	});
-	}});
+	}
+else console.log(data)});
 console.log(data,d_id);
 	return res.json("welcome user "+ uid);
 }
 }
 else
 
- return res.json("wrong input");
+ return res.json("user registered");
 });
 //console.log(data);
 	//res.send("it works");
@@ -66,12 +70,22 @@ else
 	app.post('/Home/:id',(req,res)=>
 {
 	console.log(d_id);
-
+	var point;
 	const q_id=req.params.id;
 	var {a} = req.body;
 console.log(q_id,a);
 if(d_id!=0)
 {
+	knex('ans').where({
+  u_id: d_id
+}).select('points').then(data3=>{
+point = data3[0].points;
+if(point==null)
+point=0;
+console.log("points "+point);
+})
+
+
 	knex('answers').where({
   id: q_id
 }).select('ans').then(data=>{
@@ -80,10 +94,13 @@ console.log(data);
 	{
 	if(data[0].ans==a){
 console.log(data,a);
-knex('submissions')
+
+point=point+1;
+knex('ans')
 .where('u_id','=',d_id)
 .update({
-  	ans1:1
+  	ans1:1,
+  	points :point
 }).then(data=>{})
 	return res.json("welcome user "+ q_id);
 
